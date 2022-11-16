7ba4da24cfad607c2bdcb054995dda64817a27d9
AlexanderGr-dev <alex.griesbeck@gmx.de>
2022-11-16 21:45:45 +0100

    Created new activity for adding trips.

--- app/src/main/java/com/griesbeck/travelio/TripActivity.kt
+++ app/src/main/java/com/griesbeck/travelio/TripActivity.kt
@@ -8,11 +8,15 @@ import android.view.MenuItem
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.griesbeck.travelio.databinding.ActivityTripBinding
 import androidx.core.util.Pair
+import androidx.lifecycle.ViewModelProvider
+import com.griesbeck.travelio.models.Trip
+import com.griesbeck.travelio.ui.trips.TripsViewModel
 import java.text.SimpleDateFormat
 
 class TripActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTripBinding
+    var trip = Trip()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,9 +27,21 @@ class TripActivity : AppCompatActivity() {
         setSupportActionBar(binding.toolbarTrip)
         supportActionBar?.setDisplayShowTitleEnabled(false)
 
+        val tripsViewModel =
+            ViewModelProvider(this).get(TripsViewModel::class.java)
 
-        binding.inputDate.setOnClickListener {
+
+        binding.etDate.setOnClickListener {
             setDate()
+        }
+
+        binding.btnAdd.setOnClickListener {
+            trip.location = binding.etLocation.text.toString()
+            trip.period = binding.etDate.text.toString()
+            trip.accomodation = binding.etAccomodation.text.toString()
+            trip.costs = binding.etAccomodation.text.toString()
+            tripsViewModel.addTrip(trip)
+            finish()
         }
 
 
@@ -65,7 +81,7 @@ class TripActivity : AppCompatActivity() {
             val startdate: String = formatter.format(it.first)
             val endDate: String = formatter.format(it.second)
             val period = "${startdate}   -   ${endDate}"
-            binding.inputDate.setText(period)
+            binding.etDate.setText(period)
         }
     }
 }
