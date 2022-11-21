3d5e748f90e4f328c958a37c20beb4017b9cde1e
AlexanderGr-dev <alex.griesbeck@gmx.de>
2022-11-21 17:44:12 +0100

    Added new button and imageview to activity_trip, added clicklistener to be able to choose image and registerImagePickerCallback function to be able to get selected image.
    Added picasso dependency.

--- app/src/main/java/com/griesbeck/travelio/TripActivity.kt
+++ app/src/main/java/com/griesbeck/travelio/TripActivity.kt
@@ -2,21 +2,27 @@ package com.griesbeck.travelio
 
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.griesbeck.travelio.databinding.ActivityTripBinding
 import androidx.core.util.Pair
 import androidx.lifecycle.ViewModelProvider
 import com.griesbeck.travelio.models.Trip
 import com.griesbeck.travelio.ui.trips.TripsViewModel
+import com.squareup.picasso.Picasso
 import java.text.SimpleDateFormat
 
 class TripActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityTripBinding
+    private lateinit var imageIntentLauncher : ActivityResultLauncher<Intent>
     var trip = Trip()
 
 
@@ -34,7 +40,10 @@ class TripActivity : AppCompatActivity() {
 
         if(intent.hasExtra("trip_edit")){
             edit = true
-            binding.btnAdd.text = "Save trip"
+            binding.btnAdd.text = getString(R.string.btn_save_trip)
+            if(trip.image != Uri.EMPTY) {
+                binding.btnChooseImage.text = getString(R.string.btn_change_image)
+            }
             trip = intent.extras?.getParcelable("trip_edit")!!
             bindTripEditData(trip)
         }
@@ -56,6 +65,12 @@ class TripActivity : AppCompatActivity() {
                 startActivity(tripDetailIntent)
             }
         }
+
+        binding.btnChooseImage.setOnClickListener {
+          showImagePicker(imageIntentLauncher,this)
+        }
+
+        registerImagePickerCallback()
 
 
     }
@@ -106,9 +121,41 @@ class TripActivity : AppCompatActivity() {
     }
 
     private fun bindTripEditData(trip: Trip){
+        Picasso.get().load(trip.image).into(binding.ivTripImageChoose)
         binding.etLocation.setText(trip.location)
         binding.etDate.setText(trip.period)
         binding.etAccomodation.setText(trip.accomodation)
         binding.etCosts.setText(trip.costs)
+    }
+
+    private fun registerImagePickerCallback() {
+        imageIntentLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { result ->
+                when (result.resultCode) {
+                    RESULT_OK -> {
+                        if (result.data != null) {
+
+                            val image = result.data!!.data!!
+                            contentResolver.takePersistableUriPermission(
+                                image,
+                                Intent.FLAG_GRANT_READ_URI_PERMISSION
+                            )
+                            trip.image = image
+
+                            Picasso.get()
+                                .load(trip.image)
+                                .into(binding.ivTripImageChoose)
+                            binding.btnChooseImage.text = getString(R.string.btn_change_image)
+                        }
+                    }
+                    RESULT_CANCELED -> {
+                        Toast.makeText(this, "Task cancelled", Toast.LENGTH_SHORT).show()
+                    }
+                    else -> {
+                        Toast.makeText(this, "Some error occured", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
     }
 }
